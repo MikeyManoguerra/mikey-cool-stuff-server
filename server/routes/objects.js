ceef8e2ac42ad0e7b25eba812820980e26cffbb8
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-02-04 20:57:24 -0500

    fleshed out get route minus hydrated categories, started post route

--- server/routes/objects.js
+++ server/routes/objects.js
@@ -1,24 +1,58 @@
 'use strict';
 const express = require('express');
-
 const router = express.Router();
-
-const knex = require ('../../utils/knex');
+const knex = require('../../utils/knex');
 
 
 router.get('/', (req, res, next) => {
   knex
-    .select('objects.id', 'name')
+    .select('objects.id', 'name', 'description', 
+      'image_id as imageId', 
+      'images.image_one as imageOne','images.image_two as imageTwo',
+      'images.image_three as imageThree','images.image_four as imageFour',
+      'location_id as locationId', 'locations.postal_code as postalCode',
+      'origin_id as originId', 'origins.country as countryOfOrigin',
+      'manufacturer_id as manufacturerId', 'manufacturers.corp as manufacturer')
     .from('objects')
+    .leftJoin('images', 'objects.image_id', 'images.id')
+    .leftJoin('locations', 'objects.location_id', 'locations.id')
+    .leftJoin('origins', 'objects.origin_id', 'origins.id')
+    .leftJoin('manufacturers', 'objects.manufacturer_id', 'manufacturers.id')
+    // .leftJoin('objects_categories', 'objects.id', 'objects_categories.object_id')
     .orderBy('objects.id')
-    .then( result =>{
-      if(result){
+    .then(result => {
+      if (result) {
         res.json(result);
-      } else{
-        next ();
+      } else {
+        next();
       }
     })
     .catch(err => next(err));
 });
+
+
+router.get('/:id', (req, res, next) => {
+
+});
+
+router.post( '/', (req, res, next) =>{
+  const { 
+    name,
+    description, 
+    location, 
+    countryOfOrigin, 
+    manufacturer, 
+    global, 
+    postalCode,
+  } = req.body;
+  // need to figure out what data i am collecting from images!
+
+  const newObject = {
+
+    
+  }
+
+});
+
 
 module.exports = router;
