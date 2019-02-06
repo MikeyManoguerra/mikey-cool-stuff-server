3f1fbf74ad401422dad4af34b96a9f7a311f7ddc
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-02-06 10:15:43 -0500

    resave

--- config.js
+++ config.js
@@ -1,3 +1,15 @@
 'use strict';
- 
-exports.PORT = process.env.PORT || 8080;
+require('dotenv').config();
+
+const PORT = process.env.PORT || 8080;
+const CLOUD_NAME = process.env.CLOUD_NAME;
+const API_KEY = process.env.API_KEY;
+const API_SECRET = process.env.API_SECRET;
+const CLIENT_ORIGIN =  process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+module.exports ={ 
+  PORT,
+  CLOUD_NAME,
+  API_KEY,
+  API_SECRET,
+  CLIENT_ORIGIN
+}; 
--- server/index.js
+++ server/index.js
@@ -4,11 +4,14 @@ const express = require('express');
 const app = express();
 
 const morgan = require('morgan');
-const {PORT} =require('../config');
+const {PORT, CLIENT_ORIGIN} =require('../config');
+const cors = require('cors');
 
 app.use(express.json());
 app.use(morgan('dev'));
-
+app.use(cors({
+  origin: CLIENT_ORIGIN
+}));
 
 const objectsRouter = require('./routes/objects');
 
