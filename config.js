4b60c7aa1fa8ef31bf9437523cd9554c766a4e5c
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-02-14 18:11:55 -0500

    Client origin works now hopefully

--- config.js
+++ config.js
@@ -2,14 +2,8 @@
 require('dotenv').config();
 
 const PORT = process.env.PORT || 8080;
-const CLOUD_NAME = process.env.CLOUD_NAME;
-const API_KEY = process.env.API_KEY;
-const API_SECRET = process.env.API_SECRET;
-const CLIENT_ORIGIN =  process.env.CLIENT_ORIGIN || 'http://localhost:3000';
-module.exports ={ 
+const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+module.exports = {
   PORT,
-  CLOUD_NAME,
-  API_KEY,
-  API_SECRET,
   CLIENT_ORIGIN
 }; 
